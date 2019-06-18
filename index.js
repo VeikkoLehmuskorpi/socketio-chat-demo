5a16176947d19957c98a09ed5c43202ddaefa922
VeikkoLehmuskorpi <veikko.lehmuskorpi@student.lamk.fi>
2019-06-18 09:29:51 +0300

    Implement Socket.io server

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-var app = require('express')();
-var http = require('http').createServer(app);
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(3000, function() {
+io.on('connection', socket => {
+  console.log('a user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+http.listen(3000, () => {
   console.log('listening on port 3000...');
 });
