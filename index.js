0af77fa11893592e389ee859bfe663f54b0d08e1
VeikkoLehmuskorpi <veikko.lehmuskorpi@student.lamk.fi>
2019-06-18 16:11:06 +0300

    Refactoring code

--- index.js
+++ index.js
@@ -6,47 +6,67 @@ app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/index.html`);
 });
 
+// Connection started
 io.on('connection', socket => {
-  socket.broadcast.emit('chat connect', socket.handshake.address);
-  console.log(`${socket.handshake.address} connected`);
+  const nickname = socket.nickname || 'Anonymous';
 
+  const date = new Date();
+  const hours = date.getHours() < 10 ? `0${date.getHours()}` : date.getHours();
+  const minutes =
+    date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes();
+  const seconds =
+    date.getSeconds() < 10 ? `0${date.getSeconds()}` : date.getSeconds();
+
+  const formattedTimestamp = `${hours}:${minutes}:${seconds}`;
+
+  socket.emit('chat connect', nickname, formattedTimestamp);
+
+  console.log(
+    `${formattedTimestamp} >> ${socket.handshake.address} - ${nickname} connected`
+  );
+
+  // On new nickname
   socket.on('set nickname', nickname => {
     socket.nickname = nickname;
-    const users = [];
-    users.push(nickname);
-    console.log(users);
+    socket.emit('set nickname', socket.nickname);
+
+    console.log(
+      `${formattedTimestamp} >> ${socket.handshake.address} - changed their nickname to ${nickname}`
+    );
   });
 
+  // On new message
   socket.on('chat message', msg => {
-    // const user = socket.handshake.address;
-    const user =
-      socket.nickname === 'undefined'
-        ? socket.handshake.address
-        : socket.nickname;
-    const timestamp = new Date();
-    const timeHours =
-      timestamp.getHours() < 10
-        ? `0${timestamp.getHours()}`
-        : timestamp.getHours();
-    const timeMinutes =
-      timestamp.getMinutes() < 10
-        ? `0${timestamp.getMinutes()}`
-        : timestamp.getMinutes();
-    const timeSeconds =
-      timestamp.getSeconds() < 10
-        ? `0${timestamp.getSeconds()}`
-        : timestamp.getSeconds();
-    const formattedTimestamp = `${timeHours}:${timeMinutes}:${timeSeconds}`;
-    socket.broadcast.emit('chat message', msg, user, formattedTimestamp);
-    console.log(`${user} (${formattedTimestamp}) says: ${msg}`);
+    const nickname = socket.nickname || 'Anonymous';
+    const date = new Date();
+    const hours =
+      date.getHours() < 10 ? `0${date.getHours()}` : date.getHours();
+    const minutes =
+      date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes();
+    const seconds =
+      date.getSeconds() < 10 ? `0${date.getSeconds()}` : date.getSeconds();
+
+    const formattedTimestamp = `${hours}:${minutes}:${seconds}`;
+
+    socket.emit('chat message', msg, nickname, formattedTimestamp);
+
+    console.log(
+      `${formattedTimestamp} >> ${socket.handshake.address} - ${nickname} says: ${msg}`
+    );
   });
 
+  // On disconnect
   socket.on('disconnect', () => {
-    socket.broadcast.emit('chat disconnect', socket.handshake.address);
-    console.log(`${socket.handshake.address} disconnected`);
+    const nickname = socket.nickname || 'Anonymous';
+    socket.emit('chat disconnect', socket.nickname || nickname);
+
+    console.log(
+      `${formattedTimestamp} >> ${socket.handshake.address} - ${nickname} disconnected`
+    );
   });
 });
 
+// Start the server
 http.listen(3000, () => {
   console.log('listening on port 3000...');
 });
