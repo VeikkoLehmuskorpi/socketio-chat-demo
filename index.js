89d3f047fe8f3398d4e52019ddebda6f7895f3f5
VeikkoLehmuskorpi <veikko.lehmuskorpi@student.lamk.fi>
2019-06-18 09:39:25 +0300

    Emit input messages and have server receive them

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(`${__dirname}/index.html`);
 });
 
 io.on('connection', socket => {
   console.log('a user connected');
+
+  socket.on('chat message', msg => {
+    console.log(`message: ${msg}`);
+  });
 
   socket.on('disconnect', () => {
     console.log('user disconnected');
